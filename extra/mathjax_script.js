aa4f39a77f9658c21e0ff3cf5630eaedfe20ebc0
plasmon360 <plasmon360@gmail.com>
2026-02-01 23:12:18 -0800

    Generate Pelican site

---
+++ extra/mathjax_script.js
@@ -0,0 +1,14 @@
+<script>
+window.MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['$$', '$$'], ['\\[', '\\]']],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    processHtmlClass: 'arithmatex'
+  }
+};
+</script>
+<script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js" id="MathJax-script" async></script>
